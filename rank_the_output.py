70ff051ccc641326830efac8f2d0a579109ab45b
Yoko1030 <75165033+Yoko1030@users.noreply.github.com>
2023-08-06 14:32:11 +0800

    Update rank_the_output.py

--- rank_the_output.py
+++ rank_the_output.py
@@ -8,14 +8,14 @@ def main(args):
         lines = file.readlines()
         newlines = [line.rstrip("\n").replace(" ","") for line in lines]
     if args["aug"]:
-        o_split_lines = [newlines[600 * i:600 * i + 600] for i in range(0, int(len(newlines) / 600))]
+        o_split_lines = [newlines[3000* i:3000* i + 3000] for i in range(0, int(len(newlines) / 3000))]
         split_lines = []
         for lines in o_split_lines:
-            split_line = [lines[x:x + 60] for x in range(0, 600, 60)]  #split_line [(type1,aug1,beam1),(type1,aug1,beam2)]
+            split_line = [lines[x:x + 300] for x in range(0, 3000, 300)]  #split_line [(type1,aug1,beam1),(type1,aug1,beam2)]
             t_split =[]
             for lin in split_line:
                 list1, list2, list3 = [], [], []
-                for i in range(20):
+                for i in range(100):
                     list1.append(lin[3*i])  #add the type n aug n beam1
                     list2.append(lin[3*i+1]) #add the type n aug n beam2
                     list3.append(lin[3*i+2])
@@ -34,10 +34,11 @@ def main(args):
         rank_line.append(lo.pop(0))  #top-1 beam2
         rank_line.append(lo.pop(1))  #top-2 beam2
         rank_line.append(lo.pop(2))  #top-3 beam1
-        rank_line.append(lo.pop(2))  #top-1 beam3
-        rank_line.append(lo.pop(3))
-        rank_line.append(lo.pop(3))
-        #top-3 beam1
+        rank_line.append(lo.pop(0))  #top-1 beam3
+        rank_line.append(lo.pop(3))  #top-4 beam1
+        rank_line.append(lo.pop(1))  #top-3 beam2
+        rank_line.append(lo.pop(4))  #top-5 beam1
+        rank_line.append(lo.pop(6))  #top-6 beam1
         rank_line.extend(lo)
         if args["aug"]:
             for i in range(len(rank_line)):
@@ -52,7 +53,7 @@ def main(args):
                        op+=1
                        invalid_rank_line.append(smile)
                 rank_line[i] = valid_rank_line+invalid_rank_line
-                if op==20:   # 20:the aug20X
+                if op==100:   # 20:the aug20X
                     error_smi = rank_line.pop(i)
                     rank_line.append(error_smi)
         else:
