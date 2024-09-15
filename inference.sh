488aca81e2f8cbc11e6bcd490bb057fd00b1b276
Yoko1030 <75165033+Yoko1030@users.noreply.github.com>
2024-09-15 13:15:12 +0800

    Update inference.sh

--- inference.sh
+++ inference.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 python generate_test_file.py
-python translate.py -model model/cluster77_noaug_step_100000.pt -src output/output/re_index_uspto_test_out_top10.txt -output output/output/pred_test_top3_3_norank.targets.txt     -batch_size 1024 -replace_unk -max_length 200 -beam_size 3  -n_best 3  -gpu 0
+python translate.py -model model/cluster77_noaug_step_100000.pt -src re_index_uspto_test_out_top10.txt -output output/output/pred_test_top3_3_norank.targets.txt     -batch_size 1024 -replace_unk -max_length 200 -beam_size 3  -n_best 3  -gpu 0
 python rank_the_output.py
 python cal_top_k_maxfrag_file.py
